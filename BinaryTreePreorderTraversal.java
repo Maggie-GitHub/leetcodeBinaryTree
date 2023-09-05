fb60cdd5b187db2d11485c2696e15534bac84ccc
manyu <manyu14630@gmail.com>
2023-09-05 23:58:37 +0800

    binary tree preorder
    using preorder, solve the problem, lc 144

---
+++ BinaryTreePreorderTraversal.java
@@ -0,0 +1,20 @@
+import javax.swing.tree.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePreorderTraversal {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        preorder(root, result);
+        return result;
+    }
+    public void preorder(TreeNode root, List<Integer> result){
+        if(root == null){
+            return;
+        }
+
+        result.add(root.val);
+        preorder(root.left, result);
+        preorder(root.right, result);
+    }
+}
